213362a6a32919012e7c550301aac549f80f84d3
VaricelaZoster <162027550+VaricelaZoster@users.noreply.github.com>
2025-12-29 16:11:07 +0530

    Added search bar styling

--- homeComponents/Searchbar.jsx
+++ homeComponents/Searchbar.jsx
@@ -6,9 +6,11 @@ const Searchbar = ({isVisible}) => {
     return (
         <div className='bg-blue-400 flex justify-center py-3 sticky top-0 z-10'>
             {!isVisible && (
-                <div>tenor</div>
+                <div className='flex items-center pr-6 cursor-pointer transition-all translate-x-8'>
+                    <div className='text-3xl text-white'>tenor</div>
+                </div>
             )}
-            <div className="flex bg-white w-[80%] h-15 rounded-sm shadow-md font-semibold text-xl">
+            <div className={`flex bg-white transition-all ${isVisible ? 'w-[80%]' : 'w-[70%] mr-3 translate-x-9.5'} h-15 rounded-sm shadow-md font-semibold text-xl`}>
                 <input type="text" placeholder='Search for GIFS and stickers' className="px-6 w-full" />
                 <div className="flex items-center px-2.5">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="size-8">
